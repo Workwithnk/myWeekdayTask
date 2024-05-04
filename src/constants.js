74903ed8d409c76c91c809d7be88971a07f517dc
Nikhil.Kavathiya <nkavathiya@ginnovat.com>
2024-05-04 18:54:47 +0530

    Main: Added Constant filters data & installed MUI

---
+++ src/constants.js
@@ -0,0 +1,24 @@
+export const ROLES_CONSTANTS = [
+	{ type: "Engineering", value: ["Backend", "Frontend", "Fullstack", "IOS", "Flutter", "React Native", "Android", "Tech Lead", "Dev-Ops", "data engineer", "Data Science", "Computer-Vision", "Nlp", "Test / Qa", "Web3", "Deep-Learning", "SRE", "Data-Infrastructure"] },
+	{ type: "Design", value: ["Designer", "Design Manager", "Graphic Designer", "Product Designer"] },
+	{ type: "Product", value: ["Product Manager"] },
+	{ type: "Operations", value: ["Operations Manager", "Founder’s office/Chief Of staff"] },
+	{ type: "Sales", value: ["Sales development representative", "Account executive", "Account manager"] },
+	{ type: "Marketing", value: ["Digital marketing manager ", "Growth hacker", "Marketing", "Product marketing manager"] },
+	{ type: "Other Engineering", value: ["Hardware", "Mechanical", "Systems"] },
+	{ type: "Business Analyst", value: ["Business analyst"] },
+	{ type: "Data Analyst", value: ["Data Analyst"] },
+	{ type: "Project Manager", value: ["Project Manager"] },
+	{ type: "Management", value: ["Management"] },
+	{ type: "Legal", value: ["Legal"] },
+	{ type: "HR", value: ["HR"] },
+	{ type: "Finance", value: ["Finance"] }
+];
+
+export const NUM_OF_EMP = ["1-10", "11-20", "21-50", "51-100", "101-200", "201-500", "500+"];
+
+export const EXPE = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export const REMOTE = ["Remote", "Hybrid", "In-office"];
+
+export const BASE_PAY = ["0L", "10L", "20L", "30L", "40L", "50L", "60L", "70L"];
